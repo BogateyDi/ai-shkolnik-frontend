2179cba69ad80d641a8cd14648a9ed74b761fcab
BogateyDi <125331762+BogateyDi@users.noreply.github.com>
2025-07-11 07:47:59 +0700

    Delete components/GeminiDocInterface.tsx

--- components/GeminiDocInterface.tsx
+++
@@ -1,5 +0,0 @@
-// This file is DELETED.
-// The conceptual functionality of a two-pane document interface
-// with chat interaction for Gemini DOC has been moved into App.tsx
-// as part of the DokVik module's "gemini_doc_chat" mode.
-// The DocumentInputPane component is used within App.tsx to achieve this.
